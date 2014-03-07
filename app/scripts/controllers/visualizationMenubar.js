59bbc74a537c50da1570f5a6e4081a4b8131b521
buildingJarl <ivan.bacher@gmail.com>
2014-03-07 22:25:10 +0000

    save

--- app/scripts/controllers/visualizationMenubar.js
+++ app/scripts/controllers/visualizationMenubar.js
@@ -3,9 +3,10 @@
 angular.module('esExploreApp')
 	.controller('VisualizationMenubarCtrl', function ($scope) {
 		
-		$scope.tooltip = {};
-		$scope.tooltip.home = { title:'Home'};
-		$scope.tooltip.create = ({ title:'Create'});
-		$scope.tooltip.settings = ({ title:'Settings'});
-		$scope.tooltip.help = ({ title:'Help'});
+		$scope.tooltip = {
+			home: { title:'Home' },
+			create: { title:'Create' },
+			settings: { title:'Settings' },
+			help: { title:'Help' }
+		};
 	});
